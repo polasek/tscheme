56b1a4a635f228595fe85dcd2ebf9705f3735346
Jan <polasekjan@gmail.com>
2014-04-16 20:41:41 -0400

    add analyze-type.scm

---
+++ analyze-type.scm
@@ -0,0 +1,17 @@
+(load "load-analyze")
+
+(define analyze-type
+  (make-generic-operator 2 'analyze-type
+                         (lambda (exp env)
+                           (cond ((application? exp)
+				  (analyze-application exp env))
+				 (else
+				  (error "Unknown expression type"
+					 exp env))))))
+
+(defhandler analyze-type (lambda (x env) (list 'string  env)) string?)
+(defhandler analyze-type (lambda (x env) (list 'number  env)) number?)
+(defhandler analyze-type (lambda (x env) (list 'boolean env)) (lambda (exp) (or (eq? exp #t)
+                                                                                (eq? exp #f))))
+
+(define (analyze-type-variable epx env)
