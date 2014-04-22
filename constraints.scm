ee0c72a690fe7b3ee326abe812867da78201c245
Aaron Graham-Horowitz <aarongrahamhorowitz@college.harvard.edu>
2014-04-22 19:29:17 -0400

    constraint-type definitions in constraints.scm

---
+++ constraints.scm
@@ -0,0 +1,35 @@
+
+(define (unify constraints)
+  error 'TODO)
+
+(define *requires* 'REQUIRES)
+(define *permits*  'PERMITS)
+
+(define-record-type type
+    (type:make name)
+    type?
+    (name  type:name))
+
+(define-record-type variable
+    (var:make name)
+    variable?
+    (name  var:name))
+
+(define-record-type argument
+    (arg:make name number)
+    argument?
+    (name    arg:name)
+    (number  arg:number))
+
+(define-record-type constraint
+    (constraint:make left relation right)
+    constraint?
+    (left      constraint:left)
+    (relation  constraint:relation)
+    (right     constraint:right))
+
+(define (constraint:make-require left right)
+  (constraint:make left *requires* right))
+
+(define (constraint:make-permit left right)
+  (constraint:make left *permits* right))
