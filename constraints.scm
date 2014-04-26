762b8e6f6bd423acada9c996889fd09296367445
Aaron Graham-Horowitz <aarongrahamhorowitz@college.harvard.edu>
2014-04-26 18:47:08 -0400

    more documentation for constraints.scm

--- constraints.scm
+++ constraints.scm
@@ -1,13 +1,6 @@
 ;;;; Core definitions of types and functions for working with constraints.
 
-;; Fresh type variables
-(define **type-var-counter** 0)
-(define (fresh)
-  (set! **type-var-counter** (+ **type-var-counter** 1))
-  (var:make (symbol-append 'x **type-var-counter**)))
-
-(define (unify constraints)
-  (error 'TODO))
+;;; Data structure definitions
 
 ;; Primitive type
 (define-record-type type
@@ -28,6 +21,8 @@
     (relation  constraint:relation)
     (right     constraint:right))
 
+;;; Shortcuts for important instances
+
 (define *requires* 'REQUIRES)
 (define *permits*  'PERMITS)
 
@@ -38,6 +33,14 @@
 (define *symbol*    (type:make 'symbol))
 (define *pair*      (type:make 'pair))
 (define *procedure* (type:make 'procedure))
+
+;;; Methods to construct instances
+
+;; Fresh type variables
+(define **type-var-counter** 0)
+(define (fresh)
+  (set! **type-var-counter** (+ **type-var-counter** 1))
+  (var:make (symbol-append 'x **type-var-counter**)))
 
 (define (constraint:make-require left right)
   (constraint:make left *requires* right))
